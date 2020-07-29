12ed865ed3aa542744e64071f05557041d2f7aa2
Yin-Chen Kris Huang <job.krishuang@gmail.com>
2020-07-29 02:23:16 -0700

    Phone Icon & Other Adjustments

--- js/renderContent.js
+++ js/renderContent.js
@@ -16,7 +16,8 @@ function renderData(data) {
 
   // Images
   const googleLogo = `https://upload.wikimedia.org/wikipedia/commons/a/a9/Google_Maps_icon.svg`;
-  const blueCheckmark = `/images/blue_checkmark.png`;
+  const blueCheckmark = `<img src="/images/blue_checkmark.png" style="width: 20px; vertical-align: middle"/>`;
+  const phoneIcon = `&ThickSpace;<img src="/images/phone-ring-flat.png" width="25px"/>`;
 
   if (data.indexOf("city") > 0) {
     // Correct Restaurant Data
@@ -25,30 +26,32 @@ function renderData(data) {
     const restaurantList = contentObj.restaurants.map((item) => {
       let str = `<li style="list-style: none"><div>`;
       str += `<span style="font-weight: bold; font-size: 20px">${item.name} </span>`;
-      str += `${
-        item.verified
-          ? `<img src=${blueCheckmark} style="width: 20px; vertical-align: middle"/>`
-          : ``
-      }<br/>`;
+      str += `${item.verified ? blueCheckmark : ``}<br/>`;
       str += `<a href=${item.maps} target="_blank"><em>${item.streetAddress}, ${item.city}, ${item.state}, ${item.zip}</em>
       <span>
-      <img src=${googleLogo} style="width: 25px; vertical-align: top"/></a></span>`;
+      <img src=${googleLogo} style="width: 25px; vertical-align: top"/></a></span><br/>`;
+      str += `<a href="tel:${item.phone}">${item.phone}${phoneIcon}</a><br/>`;
       str += `<p>Category: ${item.category}<br/>`;
       str += `Dine-In: ${item.dineIn === true ? "Yes" : "No"}<br/>`;
       str += `Delivery: ${item.delivery === true ? "Yes" : "No"}<br/>`;
       str += `Takeout: ${item.toGo === true ? "Yes" : "No"}`;
+      str += `${
+        item.dineIn || item.delivery || item.toGo === true
+          ? ""
+          : "<br/><span style='color: red'>Temporarily closed</span>"
+      }`;
       str += `</p></div>`;
       str += `</li>`;
 
       return str;
     });
 
-    const renderedList = restaurantList.reduce((item, init) => (init += item), "");
+    const renderedList = restaurantList.join("");
 
     const x = document.getElementById("server_response");
     x.innerHTML = `<div style="margin-left: 200px; background-image: inherite">
                   <h3>${contentObj.city}</h3>
-                  <br><img src=${blueCheckmark} style="width: 20px; vertical-align: middle"/> Verified
+                  <br>${blueCheckmark} Verified
                   <span style="display:inline; margin-bottom: 20px; justify-content: start"><p></p></span>
                   <span>${renderedList}</span>
                   </div>`;
